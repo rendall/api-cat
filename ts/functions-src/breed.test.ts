75d34444a1cc8489749ee79ab45c67397f64e16f
Rendall <rendall@gmail.com>
2019-04-07 15:53:57 +0300

    Add tests of API

--- ts/functions-src/breed.test.ts
+++ ts/functions-src/breed.test.ts
@@ -1,5 +1,19 @@
-const breedtest = require('./breed').handler
+import {getAllBreeds, getBreed, searchBreed} from './breed'
 
-test('init test', () => {
-  expect(breedtest()).toBe(true);
-});
+const ALL_BREEDS_SUBSET = ["Siamese", "Arabian Mau", "Korat"]
+const SINGLE_BREED_OBJECT = {
+    "name": "Japanese Bobtail",
+    "country": "Japan",
+    "origin": "Natural, mutation",
+    "bodyType": "Moderate",
+    "coat": "Short/long",
+    "pattern": "All but colorpoint and ticked",
+    "temperament": "Earthy"
+  }
+
+describe("cat api breed endpoint tests", () => {
+  test("smoke test", () => { expect(true).toBe(true); });
+  test("get all breeds", () => expect(getAllBreeds()).resolves.toEqual(expect.arrayContaining(ALL_BREEDS_SUBSET)))
+  test("get single breed", () => expect(getBreed("1")).resolves.toEqual(expect.objectContaining(SINGLE_BREED_OBJECT)))
+  test("search term", () => expect(searchBreed("Angora")).resolves.toHaveLength(3))
+})
--- ts/functions-src/breed.ts
+++ ts/functions-src/breed.ts
@@ -1,10 +1,14 @@
 import {Context, Handler, Callback} from 'aws-lambda'
 
+export const getAllBreeds = () => new Promise((resolve, reject) => resolve(false))
+export const getBreed = (id:string) => new Promise((resolve, reject) => resolve(false))
+export const searchBreed = (term:string) => new Promise((resolve, reject) => resolve(false))
+
 export const handler:Handler = (event, context:Context, callback:Callback) => callback( null,  {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Hello world!',
-      input: event,
+      input: event
     }),
   }
 )
