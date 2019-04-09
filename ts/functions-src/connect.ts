56736e31206cefd74d9752bf86490b971bd1bd15
Rendall <rendall@gmail.com>
2019-04-09 23:39:11 +0300

    Troubleshoot:improve connect

--- ts/functions-src/connect.ts
+++ ts/functions-src/connect.ts
@@ -7,39 +7,22 @@ const MONGODB_URI = process.env.DB_READER_CONNECTION; // or Atlas connection str
 let cachedDb:any | null = null;
 
 
-function connectToDatabase (uri:any) {
-
-  console.log('=> connect to database');
-
-
-
-  if (cachedDb) {
-
-    console.log('=> using cached database instance');
-
-    return Promise.resolve(cachedDb);
-
-  }
-
-
-
-  return MongoClient.connect(uri, {useNewUrlParser:true})
-
-    .then((db:any) => {
-
-      cachedDb = db;
-
-      return cachedDb;
-
-    });
-
+function connectToDatabase(uri:string) {
+  
+    if (cachedDb && cachedDb.serverConfig.isConnected()) {
+        console.log('=> using cached database instance');
+        return Promise.resolve(cachedDb);
+    }
+    const dbName = 'cat';
+    return MongoClient.connect(uri)
+        .then(( client:any ) => { cachedDb = client.db(dbName); return cachedDb; });
 }
 
 
 function queryDatabase (db:any) {
   console.log('=> query database');
 
-  return db.db('cat').collection('breeds').find({}).toArray()
+  return db.collection('breeds').find({}).toArray()
     .then((result:any) => { return { statusCode: 200, body: JSON.stringify(result) }; })
     .catch(( err:any ) => {
       console.log('=> an error occurred: ', err);
@@ -54,7 +37,7 @@ module.exports.handler = (event:any, context:any, callback:any) => {
 
   console.log('event: ', event);
 
-  connectToDatabase(MONGODB_URI)
+  connectToDatabase(MONGODB_URI!)
     .then(( db:any ) => queryDatabase(db))
     .then(( result:any ) => {
       console.log('=> returning result: ', result);
